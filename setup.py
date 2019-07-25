428d10d3f073f80edfb3d12b0665ca68db796f9c
Janis Lesinskis <janis@lesinskis.com>
2019-07-25 14:40:53 -0400

    Change structure

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='gilbert-frontmatter-markdown',
-      version='0.0.1',
+      version='0.0.2',
       description='Supports frontmatter metadata',
-      url='https://github.com/oadams/persephone',
+      url='https://github.com/shuttle1987/gilbert-frontmatter-markdown',
       author='Janis Lesinskis',
-      packages=['gilbert.plugins.frontmatter_markdown'],
+      packages=['gilbert.plugins'],
       classifiers = [
           'Development Status :: 4 - Beta',
       ],
