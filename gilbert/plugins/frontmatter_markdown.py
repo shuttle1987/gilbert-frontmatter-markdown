3dd0402aca5610e17d9400da177cb26825ef3024
Janis Lesinskis <janis@lesinskis.com>
2019-07-25 14:55:28 -0400

    Fix bugs with markdown rendering

--- gilbert/plugins/frontmatter_markdown.py
+++ gilbert/plugins/frontmatter_markdown.py
@@ -1,18 +1,18 @@
 from pathlib import Path
 
-from markdown import markdown
+import markdown
 
 from gilbert import Site
 from gilbert.content import Page
 
+print("Loaded the Gilbert markdown with frontmatter loader")
 
 def load_frontmatter_md(path: Path):
     """Loader for markdown files that contain frontmatter"""
     data = path.read_text(encoding='utf-8')
-    processed = markdown(self.data, output_format='html5', extensions=['meta'])
-    html = md.convert(data)
-
-    return html, processed.Meta
+    md = markdown.Markdown(extensions = ['meta'], output_format='html5')
+    processed = md.convert(data)
+    return processed, md.Meta
 
 Site.register_loader('md', load_frontmatter_md)
 
