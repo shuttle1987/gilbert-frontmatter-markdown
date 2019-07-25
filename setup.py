bc13aa56a7ad473d86804747ff65fda24076884f
Janis Lesinskis <janis@lesinskis.com>
2019-07-25 15:14:22 -0400

    Bump minor version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gilbert-frontmatter-markdown',
-      version='0.0.2',
+      version='0.0.3',
       description='Supports frontmatter metadata',
       url='https://github.com/shuttle1987/gilbert-frontmatter-markdown',
       author='Janis Lesinskis',
