883a0ec03b78500dae13d88694eead8bc775f59a
Janis Lesinskis <janis@lesinskis.com>
2019-08-02 16:26:05 -0400

    Attempt to use a class based approach for loading

--- gilbert/plugins/frontmatter_markdown.py
+++ gilbert/plugins/frontmatter_markdown.py
@@ -4,15 +4,28 @@ import markdown
 
 from gilbert import Site
 from gilbert.content import Page
+from gilbert.utils import oneshot
+
 
 print("Loaded the Gilbert markdown with frontmatter loader")
 
-def load_frontmatter_md(path: Path):
-    """Loader for markdown files that contain frontmatter"""
+class FrontmatterMarkdownPage(Page):
+    extras: list = ['meta']
+
+    @oneshot
+    def content(self):
+        extras = self.extras
+        if not extras:
+            extras = self.site.config.get('content_type', {}).get('MarkdownPage', [])
+        #import pdb; pdb.set_trace()
+        md = markdown.Markdown(extensions=extras, output_format='html5')
+        processed = md.convert(self.data)
+        return processed, md.Meta
+
+def load_frontmatter_md(path):
     data = path.read_text(encoding='utf-8')
-    md = markdown.Markdown(extensions = ['meta'], output_format='html5')
-    processed = md.convert(data)
-    return processed, md.Meta
+
+    return data, {'content_type': 'FrontmatterMarkdownPage'}
 
 Site.register_loader('md', load_frontmatter_md)
 
